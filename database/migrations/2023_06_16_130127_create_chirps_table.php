3265edd293ead60f0a64336d41508b0d47ed6621
YasankaJ <yasanka.jayawardane@car-care.se>
2023-06-16 20:59:35 +0530

    Soft Delete Chirps

--- database/migrations/2023_06_16_130127_create_chirps_table.php
+++ database/migrations/2023_06_16_130127_create_chirps_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('message');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
